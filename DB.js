7e05ebfbd73bb3b4f7cbbb9ad4b5d62b70e5a15c
sdlqm456-ctrl <sdlqm456@gmail.com>
2026-01-12 23:55:33 +0900

    DB.js

--- DB.js
+++ DB.js
@@ -1,40 +1,43 @@
 // DB 서버생성
 
-// 모듈 임팩트
+// 모듈 임포트
 const OracleDB = require("oracledb");
 
 // 조회된 데이터를 객체방식으로 바꾸기
 OracleDB.outFormat = OracleDB.OUT_FORMAT_OBJECT;
 
-const DBcinfig = {
+const DBconfig = {
   user: "scott",
   password: "tiger",
   connectString: "192.168.0.26:1521/xe",
 };
 
-// DB 접속을위한 세션을 받아오기
+// DB 접속을 위한 세션을 받아오기ㄴ
 async function getConnection() {
   try {
-    const connection = await OracleDB.get.getConnection(DBcinfig);
+    const connection = await OracleDB.getConnection(DBconfig);
     return connection;
   } catch (err) {
-    return err;
+    console.log("DB 연결 실패:", err);
+    throw err;
   }
 }
 
-// 비동기처리에서 동기방식처리로 변경하기
+// 테스트용: 비동기 처리에서 동기 방식 처리
 async function execute() {
   const qry = `INSERT INTO board (product_no, category, name, price, stock, img)
               VALUES ('1001','음료', '코카콜라 1.5L', 1200, 10, 'cola.webp')`;
   try {
-    const connection = await OracleDB.getConnection(DBcinfig);
+    const connection = await OracleDB.getConnection(DBconfig);
     const result = await connection.execute(qry);
-    connection.commit();
+    await connection.commit();
 
     console.log("DB 등록 OK");
     console.log(result);
+    await connection.close(); // 연결 종료
   } catch (err) {
     console.log(`예외발생 => ${err}`);
   }
 }
-module.exports = { getConnection, execute };
+
+module.exports = { getConnection };
